f722fae27c1e7e69fc0cfb215963766a0a9967b6
Gabriel Azevedo <62160559+Dargouls@users.noreply.github.com>
2026-03-03 13:34:31 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/superminichat',
-  async redirects() {
-    return [
-      {
-        // Tenta capturar quem chega no domínio antigo
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'superminichat.vercel.app', // DOMÍNIO ANTIGO
-          },
-        ],
-        destination: 'https://www.gabrielzv.com/superminichat/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
